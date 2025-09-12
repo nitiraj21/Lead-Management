1b208839d3af5402667f3dc4e0819496b88a89b4
nitiraj_21 <nitirajsc21@gmail.com>
2025-09-12 16:57:53 +0530

    add tailwind css and themes

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+/** /tailwind.config.cjs */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: { extend: {} },
+  plugins: [],
+}
