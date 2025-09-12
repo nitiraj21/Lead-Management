803ad06f5db2d00ee0fdbdcbd9722293dee295b4
nitiraj_21 <nitirajsc21@gmail.com>
2025-09-13 04:37:33 +0530

    fix fe errors

--- frontend/src/hooks/useContent.tsx
+++
@@ -1,6 +0,0 @@
-import { Axios } from "axios";
-import { useState, useEffect } from "react";
-
-export default function useContent(){
-    
-}
--- frontend/src/pages/CreateLead.tsx
+++ frontend/src/pages/CreateLead.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios, { Axios } from "axios";
+import axios  from "axios";
 import { useNavigate } from "react-router-dom";
 
 
