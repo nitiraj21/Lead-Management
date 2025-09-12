87a9f49433346bd4ced9a74563175fb4fbbd2655
nitiraj_21 <nitirajsc21@gmail.com>
2025-09-13 04:45:05 +0530

    fix cors

--- backend/src/authControllers/signin.ts
+++ backend/src/authControllers/signin.ts
@@ -25,9 +25,8 @@ export const signin = async(req : Request, res : Response) =>{
 
         res.cookie("token", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: "strict",
-            maxAge: 24 * 60 * 60 * 1000 // 1 day
+            secure: true,
+            sameSite : "none"
           });
 
         return res.status(200).json({ message: "Login successful" });
